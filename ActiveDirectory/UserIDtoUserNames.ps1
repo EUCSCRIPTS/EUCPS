b6ce9fbbabb28e9723ebf4390dd6dfb0f1c48b80
EUCSCRIPTS <69645055+EUCSCRIPTS@users.noreply.github.com>
2020-08-13 23:37:30 +0530

    Create UserIDtoUserNames.ps1

---
+++ ActiveDirectory/UserIDtoUserNames.ps1
@@ -0,0 +1,6 @@
+#OneLiner to convert User ID to User Names
+
+Import-Csv "C:\Temp\UserID.csv" | Select-Object  @{Name="SamAccountName";Expression={$_.UserId}} | 
+Select -ExpandProperty SamAccountName | 
+%{Get-ADUser -Filter {SamAccountName -eq $_} -Properties SamAccountName,DisplayName} | 
+Select-Object SamAccountName,DisplayName| Export-csv "C:\Temp\UserName.csv" -Append -Notypeinformation 
